f62eb55f4b77dd9c67779fc6eabc088a93d24df3
M0JXD <jamiedrinkell@gmail.com>
2026-01-24 01:20:07 +0000

    Update FreeBSD

--- freebsd-install.sh
+++ freebsd-install.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-# WIP installer
+echo -e "Stupid simple/dangerous AXE-FX II installer for FreeBSD. If you continue it's presumed you agree to the license.\n"
+echo -e "You will need to be root and should have already installed fxload.\n"
+read -r -p "Continue? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY])
+		cp -vf axefx2load.hex /usr/local/share/fxload/
+		cp -vf fractal.conf /usr/local/etc/devd/
+        ;;
+    *)
+		exit 1
+        ;;
+esac
 
-# Note - while I can get FreeBSD to get the firmware uploaded with fxload,
-# the soundcard is not detected unless the USB is replugged after.
-echo -e "Stupid simple/dangerous AXE-FX II installer for FreeBSD.\nYou will need to be root and should have already installed fxload."
-cp -vf axefx2load.hex /usr/local/share/fxload/
-cp -vf fractal.conf /usr/local/etc/devd/
+
+
