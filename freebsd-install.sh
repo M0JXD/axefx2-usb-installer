2ea3e71c11c8223efc0dadf9da69d2a9c74a315a
M0JXD <jamiedrinkell@gmail.com>
2026-01-24 01:22:34 +0000

    Formatting

--- freebsd-install.sh
+++ freebsd-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo -e "Stupid simple/dangerous AXE-FX II installer for FreeBSD. If you continue it's presumed you agree to the license.\n"
+echo -e "Stupid simple/dangerous AXE-FX II installer for FreeBSD.\nIf you continue it's presumed you agree to the license."
 echo -e "You will need to be root and should have already installed fxload.\n"
 read -r -p "Continue? [y/N] " response
 case "$response" in
